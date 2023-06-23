f7bb87da1b650ef412323ada21bb07f7f585900d
Vaascoo <vasco.correia@rnl.tecnico.ulisboa.pt>
2023-06-23 23:20:26 +0100

    Add flakes

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, mach-nix, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (python3.withPackages (ps: with ps; [
+              requests
+              fire
+            ]))
+          ];
+        };
+      }
+    );
+}
